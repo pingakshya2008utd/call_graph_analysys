f7a7ede66ed10d38c73fad51db46d6cc5f861ff9
pingakshya2008utd <pingakshya2008@gmail.com>
2020-03-11 15:50:12 -0500

    final code generating data

--- call_graph_analysys/main.cpp
+++ call_graph_analysys/main.cpp
@@ -31,62 +31,72 @@ typedef boost::graph_traits<graph_t>::vertex_descriptor vertex_t;
 
 int main() {
 	vector <string> func_vec{ "{adpcm_main}","{upzero}", "{quantl}", "{encode}","{decode}" };
-	graph_t graphviz;
-	boost::dynamic_properties dp(boost::ignore_other_properties);
 
-	//	dp.property("node_id", boost::get(&DotVertex::name, graphviz));
-	dp.property("label", boost::get(&DotVertex::label, graphviz));
-	//dp.property("peripheries", boost::get(&DotVertex::peripheries, graphviz));
-	dp.property("label", boost::get(&DotEdge::label, graphviz));
-	std::ifstream dot("O:/total_new_apdcm/total_new_solution7/callgraph.dot");
-	//std::ifstream dot("C:/Users/pxg131330/Downloads/acmart-master/cfg.main.dot");
-	//auto colormap = boost::make_vector_property_map<boost::default_color_type>(indexmap);
+	for (int i = 3; i < 100; i++) {
+		graph_t graphviz;
+		boost::dynamic_properties dp(boost::ignore_other_properties);
 
-	auto indexmap = boost::get(boost::vertex_index, graphviz);
-	auto colormap = boost::make_vector_property_map<boost::default_color_type>(indexmap);
+		//	dp.property("node_id", boost::get(&DotVertex::name, graphviz));
+		dp.property("label", boost::get(&DotVertex::label, graphviz));
+		//dp.property("peripheries", boost::get(&DotVertex::peripheries, graphviz));
+		dp.property("label", boost::get(&DotEdge::label, graphviz));
+		std::ifstream dot("O:/total_new_apdcm/total_new_solution" + to_string(i) +"/callgraph.dot");
+	
 
 
-	bool status = boost::read_graphviz(dot, graphviz, dp);
-	if (status)
-		cout << 255;
 
-	map <string, vector<string>> graph_map;
+		bool status = boost::read_graphviz(dot, graphviz, dp);
+		if (status)
+			cout << 255;
 
-	for (auto v : make_iterator_range(vertices(graphviz))) {
-		string parent_name = graphviz[v].label;
+		map <string, vector<string>> graph_map;
+		string file_path_name = "O:/total_new_apdcm/total_new_solution" + to_string(i) + "/call_graph_detail.gph";
+		cout << file_path_name << endl;
+		std::ofstream outfile(file_path_name);
 
-		if (std::find(func_vec.begin(), func_vec.end(), parent_name) != func_vec.end()) {
-			std::cout << "Element found" << endl;
+		for (auto v : make_iterator_range(vertices(graphviz))) {
+			string parent_name = graphviz[v].label;
 
-			vector <string> child_vec;
-			for (auto w : make_iterator_range(adjacent_vertices(v, graphviz))) {
+			if (std::find(func_vec.begin(), func_vec.end(), parent_name) != func_vec.end()) {
+			//	std::cout << "Element found" << endl;
 
-				string child_name = graphviz[w].label;
+				vector <string> child_vec;
+				for (auto w : make_iterator_range(adjacent_vertices(v, graphviz))) {
 
-				if (std::find(func_vec.begin(), func_vec.end(), child_name) != func_vec.end()) {
-					child_vec.push_back(child_name);
+					string child_name = graphviz[w].label;
+
+					if (std::find(func_vec.begin(), func_vec.end(), child_name) != func_vec.end()) {
+						child_vec.push_back(child_name);
+					}
+
+
 				}
 
+				graph_map.emplace(parent_name, child_vec);
+				child_vec.clear();
+
+			}
+		}
+
+		for (auto& x : graph_map) {
+
+		//	cout << x.first << "-> ";
+
+			outfile << "garph " << x.first << "-> ";
+
+			for (int i = 0; i < x.second.size(); i++) {
+				//cout << x.second[i] << ",";
+				outfile << x.second[i] << ",";
 				
 			}
-
-			graph_map.emplace(parent_name, child_vec);
-			child_vec.clear();
+		//	cout << endl;
+			outfile << endl;
 
 		}
+
+		outfile.close();
+		graph_map.clear();
 	}
-	
-	for (auto& x : graph_map) {
-
-		cout << x.first << "-> ";
-
-		for (int i = 0; i < x.second.size(); i++) {
-			cout << x.second[i] << ",";
-		}
-		cout << endl;
-
-	}
-		
 
 	system("pause");
 	return 0;
